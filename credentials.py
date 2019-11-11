4ace2e0d0b1f49eb08e36d846d06e62cd5fa3175
manuel.bit <emmanuel_njuguna@outlook.com>
2019-11-11 13:51:02 +0300

    add coppy functionality

--- credentials.py
+++ credentials.py
@@ -1,3 +1,6 @@
+import random
+import string
+import pyperclip
 class Credentials:
   credentials_list = []
   '''
@@ -16,4 +19,34 @@ class Credentials:
     frunction that enables a user to delete a credential
     '''
     Credentials.credentials_list.remove(self)
+
+  @classmethod
+  def find_by_app_name(cls,appName):
+    '''
+    method that takes in application name and returns its details
+    '''
+    for credentials in cls.credentials_list:
+      if credentials.appName == appName:
+        return credentials
+
+  @classmethod
+  def display_credentials(cls):
+    '''
+    method tha returns a list of all credentials
+    '''
+    return cls.credentials_list
+
+  @classmethod
+  def generate_password(cls):
+    '''
+    method that generates password
+    '''
+    password = string.ascii_lowercase
+    return "".join(random.choice(password)for i in range (10))
+
+  @classmethod
+  def coppy(cls,appName):
+    found_app = Credentials.find_by_app_name(appName)
+    pyperclip.copy(found_app.appName)
+
 
